b1dd1a043a760d03b17f9e7f93771abee320eae4
Gerar Francis <fryzito@users.noreply.github.com>
2015-01-28 15:22:25 -0500

    Create problema2015A.cpp

---
+++ problema2015A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define vi vector<int>
+#define ii pair<int,int>
+#define vii vector<ii>
+#define REPN(i,x,y) for(int i=x;i<y;i++)
+#define REP(i,y) REPN(i,0,y)
+#define REPR(i,y,x) for(int i=y;i>=x;i--)
+#define CLR(A,x) memset(A,x,sizeof(A))
+#define INF (1<<30)
+#define EPS (1e-9)
+#define ALL(v) (v).begin(),(v).end()
+#define RALL(v) (v).rbegin(),(v).rend()
+#define pb push_back
+#define mp make_pair
+#define sqr(x) (x)*(x)
+#define dbg(x) cout << #x << " = " << x << endl
+#define dbg2(x,y)cout<<#x<<"="<<x<<" "<<#y<<"="<<y<<endl
+#define dbg3(x,y,z)cout<<#x<<"="<<x<<" "<<#y<<"="<<y<<" "<<#z<<"="<<z<<endl
+#define S(x)scanf("%d\n",&x)
+#define SS(str)scanf("%[^\n]\n",str)
+#define S2(x,y)scanf("%d %d\n",&x,&y)
+#define SC(x)scanf("%d",&x)
+#define SC2(x,y)scanf("%d %d",&x,&y)
+#define P(x)printf("%d\n",x)
+#define SZ(v) v.size()
+#define f first
+#define s second
+#define MOD 100007
+#define MAXN 103
+using namespace std;
+int main(){
+	unsigned long long int fac[100];
+	fac[0]=1;
+	fac[1]=1;
+	for(int i=2;i<=60;i++){
+	fac[i]=(unsigned long long int)i*fac[i-1];
+		int aux=(((fac[i-1]+1) % i) == 0);
+		dbg2(i,aux);
+	}
+
+	return 0;
+}
