2ef181ec1c2ec513f52083abdd299faede743a69
Gerar Quispe <fryzito@gmail.com>
2024-11-30 17:46:18 -0500

    Adding updating of problem of UNSA contest.Desk-20241130

--- semana-aniversario-UNSA_A_codeforces.cpp
+++ semana-aniversario-UNSA_A_codeforces.cpp
@@ -94,16 +94,7 @@ int counting(int n){
 }
 int main(){
     int n;
-    vector<int>v;
-    v.push_back(1);
-    v.push_back(100);
-    v.push_back(599);
-    v.push_back(1000);
-    v.push_back(10000);
-    for(int i=0;i<=v.size();i++){
-        //S(v[i]);
-        P(counting(v[i]));
-    }
-
+    S(n);
+    P(counting(n));
     return 0;
 }
