009256f9d8d0f5fa2654ef5642cab96811518bb9
Gerar Quispe <fryzito@gmail.com>
2025-07-12 09:19:20 -0500

    feat(javascript):adding more code for practice.desk20250712

--- javascript/book-products.js
+++ javascript/book-products.js
@@ -1,12 +1,26 @@
-import products from './products.js'
+import producto from './products.js'
 
 console.log('tamaño de productos:')
-console.log('# de productos: ', products.length);
+console.log('# de productos: ', producto.length);
 
 // buscar un producto por marca:
-const appleProduct = products.filter(
+const appleProduct = producto.filter(
     p => p._id == 3
 )
 
-console.log(appleProduct)
+//console.log(appleProduct)
+
+const A = [0,1,2,3,4,5,6]
+
+const values = ['lunes','martes','miercoles','jueves','viernes','sabado','domingo']
+
+const newArray = A.map(value => values[value])
+
+//console.log(newArray)
+
+const dias = ["Lunes", "Martes", "Miércoles"];
+const resultado = dias.join(" - ");
+console.log(resultado); // "Lunes - Martes - Miércoles"
+
+
 
