7ae5838501ba4a3ff6c6e58bb8bcf558bf5e2ea4
Gerar Quispe <fryzito@gmail.com>
2026-04-01 15:28:19 -0500

    code(javascript):new code javascript.lapt20260401

---
+++ javascript/looping.js
@@ -0,0 +1,15 @@
+
+console.log("Hola javaScript how to looping in js");
+console.log("for for loop");
+const n = 10;
+let i = 0;
+while (i < n){
+    i = i + 1
+    console.log(i)
+}
+
+console.log("for while loop")
+for (let i = 0; i < n; i++){
+    console.log(i)
+}
+
